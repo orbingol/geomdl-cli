52906b4baa0b8b40e3b215b93e0e8db70fe5bdf6
Onur Rauf Bingol <orbingol@gmail.com>
2018-11-04 10:38:24 -0600

    Update command help texts

--- geomdl/cli/runner.py
+++ geomdl/cli/runner.py
@@ -31,17 +31,43 @@ from . import helpers_nurbs
 
 
 def command_help(**kwargs):
-    """HELP: Displays GEOMDL-CLI help"""
+    """\
+HELP: Displays GEOMDL-CLI help\
+    """
     print(__usage__)
 
 
 def command_version(**kwargs):
-    """VERSION: Displays GEOMDL-CLI version"""
+    """\
+VERSION: Displays GEOMDL-CLI version\
+    """
     print("GEOMDL-CLI version", __version__)
 
 
 def command_plot(yaml_file, **kwargs):
-    """PLOT: Plots the input NURBS curve(s) or surface(s)"""
+    """\
+PLOT: Plots single or multi NURBS curves or surfaces
+
+"geomdl plot" command takes a YAML file as the input. The YAML file can contain single or multiple shapes. \
+The YAML file format is described in the GEOMDL-CLI documentation.
+
+Alternatively, NURBS-Python library may be used to export curves and surfaces as YAML files. \
+Please see "geomdl.exchange.export_yaml" documentation for details.
+
+Usage:
+
+    geomdl plot {yaml file}                             plots the shape defined in the YAML file
+    geomdl plot {yaml file} --delta=0.1                 plots the shape using the evaluation delta of 0.1
+    geomdl plot {yaml file} --index=2                   plots the 2nd shape defined in the YAML file
+    geomdl plot {yaml file} --index=1 --delta=0.025     plots the 1st shape using the evaluation delta of 0.025
+
+Available parameters:
+    --help      displays this message
+    --index=n   plots n-th curve or surface in the YAML file (works only for multi shapes)
+    --delta=d   allows customization of the pre-defined evaluation delta in the YAML file. 0.0 < d < 1.0
+
+Please see GEOMDL-CLI documentation for details.\
+    """
     # Get keyword arguments
     shape_idx = kwargs.get('index', -1)
     shape_delta = kwargs.get('delta', -1.0)
