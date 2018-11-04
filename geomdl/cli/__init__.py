b22a23888974094fc82a6060bc48e033af5e18b1
Onur Rauf Bingol <orbingol@gmail.com>
2018-11-04 15:12:59 -0600

    Update help text

--- geomdl/cli/__init__.py
+++ geomdl/cli/__init__.py
@@ -38,8 +38,8 @@ Usage:
 
 Available commands:
 
-    help        displays geomdl-cli help
-    version     displays geomdl-cli version
+    help        displays this message
+    version     displays the package version
     plot        plots single or multiple NURBS curves and surfaces using matplotlib
     eval        evaluates NURBS shapes and exports the evaluated points in various formats
 
